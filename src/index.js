cac765a8d467804aa6fa47849dd46a71091eeba2
Jason Kaufman <jkaufman217@gmail.com>
2021-05-03 22:18:04 -0700

    Created the board, paddles, and ball.
    Added event listener to wait for up/down arrow key presses.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Ball(props) {
+    return (
+        <div className='ball'>
+            <ol>Ball</ol>
+        </div>);
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Paddle(props) {
+    return (
+        <div className='paddle'>
+            <ol>Paddle {props.value}</ol>
+        </div>
+    );
+}
+
+class Board extends React.Component {
+    renderPaddle(i) {
+        return (
+            <Paddle
+                value={i}
+            />
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <div id='paddle0' style={{ position: 'absolute', zIndex: '10', top: '225px', left: '4px' }}>
+                    {this.renderPaddle(0)}
+                </div>
+                <div id='paddle1' style={{ position: 'absolute', zIndex: '10', top: '225px', right: '4px' }}>
+                    {this.renderPaddle(1)}
+                </div>
+                <div style={{ position: 'absolute', zIndex: '20', top: '247px', left:'50px' }}>
+                    <Ball />
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.focusRef = React.createRef();
+
+        // Setup event handler for keyboard input.
+        this.keyPress = this.keyPress.bind(this);
+    }
+
+    // Set focus at first render.
+    componentDidMount() {
+        this.focusRef.current.focus();
+    }
+
+    //Set focus anytime a component updates.
+    componentDidUpdate() {
+        this.focusRef.current.focus();
+    }
+
+    // Handle user input.
+    keyPress(e) {
+        if (e.keyCode === 38) {
+            console.log("Up was pressed");
+            document.getElementById('paddle0').innerHTML = "hello";
+        } else if (e.keyCode === 40) {
+            console.log("Down was pressed");
+            document.getElementById('paddle0').innerHTML = "no"
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div className='gameboard' style={{ position: 'absolute' }} tabIndex="0" ref={this.focusRef} onKeyDown={(e) => this.keyPress(e)}>
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
