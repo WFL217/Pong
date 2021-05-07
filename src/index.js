05f990137a55f4a3228bc968529aac4fe99a92fe
Jason Kaufman <jkaufman217@gmail.com>
2021-05-07 16:22:19 -0700

    Refactored some Paddle code.

--- src/index.js
+++ src/index.js
@@ -2,39 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+console.log("sin of 30 degrees ", Math.sin(30 * Math.PI/180));
+
 function Ball(props) {
     return (
         <div className='ball'>
             <ol>Ball</ol>
-        </div>);
-}
-
-function Paddle(props) {
-    return (
-        <div className='paddle'>
-            <ol>Paddle {props.value}</ol>
         </div>
     );
 }
 
-class Board extends React.Component {
-    renderPaddle(i) {
-        return (
-            <Paddle
-                value={i}
-            />
-        );
+class Paddle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value,
+            position: props.position,
+            top: props.top,
+            left: props.left,
+            zIndex: props.zIndex,
+        };
     }
 
     render() {
         return (
+            <div className='paddle' style={{ position: this.state.position, zIndex: this.state.zIndex, top: this.state.top, left: this.state.left }}>
+                <ol>Paddle {this.state.value}</ol>
+            </div>
+        );
+    }
+}
+
+
+class Board extends React.Component {
+    render() {
+        return (
             <div>
-                <div id='paddle0' style={{ position: 'absolute', zIndex: '10', top: '225px', left: '4px' }}>
-                    {this.renderPaddle(0)}
-                </div>
-                <div id='paddle1' style={{ position: 'absolute', zIndex: '10', top: '225px', right: '4px' }}>
-                    {this.renderPaddle(1)}
-                </div>
+                <Paddle value={0} position={'absolute'} zIndex={'10'} top={'225px'} left={'4px'} />
+                <Paddle value={1} position={'absolute'} zIndex={'10'} top={'225px'} left={'786px'} />
                 <div style={{ position: 'absolute', zIndex: '20', top: '247px', left:'50px' }}>
                     <Ball />
                 </div>
@@ -67,10 +72,10 @@ class Game extends React.Component {
     keyPress(e) {
         if (e.keyCode === 38) {
             console.log("Up was pressed");
-            document.getElementById('paddle0').innerHTML = "hello";
+            //document.getElementById('paddle0').innerHTML = "hello";
         } else if (e.keyCode === 40) {
             console.log("Down was pressed");
-            document.getElementById('paddle0').innerHTML = "no"
+            //document.getElementById('paddle0').innerHTML = "no";
         }
     }
 
