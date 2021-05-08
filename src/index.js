bd01e4e61c7f549ed67feebdcc86d3bb08a61f4d
Jason Kaufman <jkaufman217@gmail.com>
2021-05-08 00:09:25 -0700

    Added movement for the left paddle.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-console.log("sin of 30 degrees ", Math.sin(30 * Math.PI/180));
+console.log("sin of 30 degrees ", Math.sin(30 * Math.PI / 180));
+
+const PADDLE_SPEED = 10;
 
 function Ball(props) {
     return (
@@ -15,47 +17,45 @@ function Ball(props) {
 class Paddle extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            value: props.value,
-            position: props.position,
-            top: props.top,
-            left: props.left,
-            zIndex: props.zIndex,
-        };
+
+        console.log("Paddle top value:", this.props.top);
     }
 
     render() {
         return (
-            <div className='paddle' style={{ position: this.state.position, zIndex: this.state.zIndex, top: this.state.top, left: this.state.left }}>
-                <ol>Paddle {this.state.value}</ol>
+            <div className='paddle' id={'paddle' + this.props.value} style={{ position: this.props.position, zIndex: this.props.zIndex, top: this.props.top + 'px', left: this.props.left }}>
+                <ol>Paddle {this.props.value}</ol>
             </div>
         );
     }
 }
 
 
-class Board extends React.Component {
-    render() {
-        return (
-            <div>
-                <Paddle value={0} position={'absolute'} zIndex={'10'} top={'225px'} left={'4px'} />
-                <Paddle value={1} position={'absolute'} zIndex={'10'} top={'225px'} left={'786px'} />
-                <div style={{ position: 'absolute', zIndex: '20', top: '247px', left:'50px' }}>
-                    <Ball />
-                </div>
+function Board(props) {
+    return (
+        <div>
+            <Paddle value={0} position={'absolute'} zIndex={'10'} left={'4px'} top={props.leftPaddleTop}/>
+            <Paddle value={1} position={'absolute'} zIndex={'10'} left={'786px'} top={props.rightPaddleTop}/>
+            <div style={{ position: 'absolute', zIndex: '20', top: '247px', left: '50px' }}>
+                <Ball />
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 class Game extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            leftPaddleTop: 225,
+            rightPaddleTop: 225,
+        }
+        
         this.focusRef = React.createRef();
 
         // Setup event handler for keyboard input.
-        this.keyPress = this.keyPress.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+        document.addEventListener('keydown', this.handleKeyPress);
     }
 
     // Set focus at first render.
@@ -63,27 +63,33 @@ class Game extends React.Component {
         this.focusRef.current.focus();
     }
 
-    //Set focus anytime a component updates.
+    /*//Set focus anytime a component updates.
     componentDidUpdate() {
         this.focusRef.current.focus();
-    }
+    }*/
 
     // Handle user input.
-    keyPress(e) {
+    handleKeyPress(e) {
         if (e.keyCode === 38) {
             console.log("Up was pressed");
-            //document.getElementById('paddle0').innerHTML = "hello";
+            let currentPosition = this.state.leftPaddleTop;
+            this.setState({
+                leftPaddleTop: currentPosition - PADDLE_SPEED,
+            });
         } else if (e.keyCode === 40) {
             console.log("Down was pressed");
-            //document.getElementById('paddle0').innerHTML = "no";
+            let currentPosition = this.state.leftPaddleTop;
+            this.setState({
+                leftPaddleTop: currentPosition + PADDLE_SPEED,
+            });
         }
     }
 
     render() {
         return (
             <div>
-                <div className='gameboard' style={{ position: 'absolute' }} tabIndex="0" ref={this.focusRef} onKeyDown={(e) => this.keyPress(e)}>
-                    <Board />
+                <div className='gameboard' style={{ position: 'absolute' }} tabIndex="0" ref={this.focusRef}>
+                    <Board leftPaddleTop={this.state.leftPaddleTop} rightPaddleTop={this.state.rightPaddleTop}/>
                 </div>
             </div>
         );
