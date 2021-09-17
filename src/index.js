b733267c8da21351117d2e4e39bc8be6b0f8cd24
Jason Kaufman <jkaufman217@gmail.com>
2021-09-17 12:11:12 -0700

    Added hyperlink to GitHub to menu.

--- src/index.js
+++ src/index.js
@@ -54,7 +54,12 @@ function Result(props) {
 function Menu(props) {
     return (
         <div className='menu'>
-            <button disabled={props.buttonDisabled} onClick={props.onClick}>{props.buttonText}</button>
+            <div>
+                <button disabled={props.buttonDisabled} onClick={props.onClick}>{props.buttonText}</button>
+            </div>
+            <div style={{ position: 'absolute', bottom: '10%', left: '0%', right: '0%', margin: '0 auto' }}>
+                <a href='https://github.com/WFL217/Pong' target='_blank' rel='noreferrer'>View on GitHub!</a>
+            </div>
         </div>
     );
 }
